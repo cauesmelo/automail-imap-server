61098be3846865940d549acbc007f4b0d92f180f
Cauê Melo <cauesmelo@gmail.com>
2020-12-03 21:09:27 -0300

    Listener and request for api done

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,78 @@
 import { simpleParser } from 'mailparser';
-import Imap, { ImapMessage, Box } from 'imap';
+import Imap, { ImapMessage } from 'imap';
 import 'dotenv/config';
+import api from './services/api';
 
-const imap = new Imap({
-  user: process.env.USERNAME || '',
-  password: process.env.PASSWORD || '',
-  host: process.env.HOST,
-  port: 143,
-  tls: false,
-});
-
-const searchUnread = () => {
-  imap.search(['UNSEEN'], (err: Error, results) => {
-    if (err) throw err;
-
-    const search = imap.fetch(results, { bodies: '' });
-
-    search.on('message', (msg: ImapMessage) => {
-      msg.on('body', (stream, info) => {
-        let buffer = '';
-        stream.on('data', chunk => {
-          buffer += chunk.toString('utf8');
-        });
-        stream.once('end', async () => {
-          const message = await simpleParser(buffer);
-          // console.log(`Enviado por: ${message.from?.text}`);
-          console.log(`Mensagem: \n ${message.subject}`);
-        });
-      });
-    });
+setInterval(() => {
+  const imap = new Imap({
+    user: process.env.USERNAME || '',
+    password: process.env.PASSWORD || '',
+    host: process.env.HOST,
+    port: 143,
+    tls: false,
   });
-};
 
-imap.once('ready', () => {
-  console.log('Imap connected.');
+  // imap.setFlags([uuids], ['\Seen'], cb)
+  // para marcar como lido
 
-  imap.openBox('INBOX', false, (err: Error, mailbox: Box) => {
-    if (err) throw new Error('Error opening email box.');
-    searchUnread();
+  const searchUnread = () => {
+    console.log('Searching for unread emails...');
+    imap.search(['UNSEEN'], (err: Error, results) => {
+      if (err) throw err;
+      try {
+        const search = imap.fetch(results, { bodies: '' });
 
-    imap.on('mail', (numMSG: number) => {
-      console.log(`Novas mensagens: ${numMSG}`);
+        search.on('message', (msg: ImapMessage) => {
+          msg.on('body', stream => {
+            let buffer = '';
+            stream.on('data', chunk => {
+              buffer += chunk.toString('utf8');
+            });
+            stream.once('end', async () => {
+              const message = await simpleParser(buffer);
+              console.log(
+                `Unread email found => from ${message.from?.text} | To: ${message.to?.text}`,
+              );
+
+              const email = message.from?.value[0];
+
+              const { date } = message;
+
+              console.log(date?.toLocaleString());
+              api
+                .post('/recipients', {
+                  msgId: message.messageId,
+                  subject: message.subject,
+                  fromEmail: email?.address,
+                  toEmail: message.to?.text,
+                  sentDate: date,
+                })
+                .catch(() => {
+                  console.log('[!!!] Error in the request!!!');
+                });
+            });
+          });
+        });
+      } catch (error) {
+        console.log('No e-mails to fetch.');
+      }
+    });
+  };
+
+  imap.once('ready', () => {
+    console.log('[Imap connected.]');
+
+    imap.openBox('INBOX', false, (err: Error) => {
+      if (err) throw new Error('[!!!] Error opening email box.');
+
       searchUnread();
+      // imap.end();
     });
   });
-});
 
-imap.connect();
+  imap.on('end', () => {
+    console.log('[Connection closed.]');
+  });
+
+  imap.connect();
+}, 5000);
